7338b953128c3fd38927aa80f5b00dee20385c06
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2021-12-18 19:40:52 +0330

    dsa
    dsa

--- pm/settings.py
+++ pm/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'mihn3y3afcnbb4)%lq5xpss%2mdi^a#60b4i_=@j5c56j9n09-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.2.175','127.0.0.1','192.168.1.50','172.17.153.145','172.16.2.149']
+ALLOWED_HOSTS = ['192.168.2.175','127.0.0.1','192.168.1.50','172.17.153.145','172.16.2.149','192.168.183.130']
 
 
 # Application definition
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cmms.apps.CmmsConfig',
     'widget_tweaks',
-	
+
     'mathfilters',
      'rest_framework',
      'django.contrib.humanize',
