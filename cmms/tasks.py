158fc7668077a1deb9ae9b970112647c64e51b64
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2021-12-22 19:34:09 +0330

    dsa
    dsa

--- cmms/tasks.py
+++ cmms/tasks.py
@@ -1,6 +1,6 @@
 # Create your tasks here
 
-from demoapp.models import Widget
+from cmms.models import WorkOrder
 
 from celery import shared_task
 
@@ -22,13 +22,13 @@ def xsum(numbers):
 
 @shared_task
 def count_widgets():
-    return Widget.objects.count()
+    return WorkOrder.objects.count()
 
 
 @shared_task
 def rename_widget(widget_id, name):
-    w = Widget.objects.get(id=widget_id)
-    w.name = name
+    w = WorkOrder.objects.get(id=widget_id)
+    # w.name = name
     w.save()
 @shared_task
 def send_email_report():
