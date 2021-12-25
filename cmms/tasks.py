369735929b9c123f14aed9a27830840ff32ca8db
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2021-12-25 19:24:37 +0330

    Update tasks.py

--- cmms/tasks.py
+++ cmms/tasks.py
@@ -2,6 +2,7 @@
 
 from celery import shared_task
 from cmms.models import *
+from django.contrib.admin.models import LogEntry, ADDITION,CHANGE,DELETION
 
 
 @shared_task
@@ -23,6 +24,14 @@ def xsum(numbers):
 @shared_task
 def send_email_report():
     print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    wos=schedule.objects.all()
+    wos=Schedule.objects.all()
     print(wos.schNextWo)
     print(wos.schnextTime)
+    LogEntry.objects.log_action(
+        user_id         = 1,
+        content_type_id = 1,
+        object_id       = 1,
+        object_repr     = 'celery',
+        action_flag     = ADDITION,
+        change_message= '1221'
+    )
