81a185dc4e28463fae9c7459fe9fc17e7523fe59
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2023-04-20 14:55:41 +0330

    Update settings.py

--- pm/settings.py
+++ pm/settings.py
@@ -199,7 +199,6 @@ LANGUAGE_CODE = 'en-us'
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'cmms/static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 LOGIN_REDIRECT_URL = 'list_dashboard'
