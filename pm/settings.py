a6570e759af971733118675806f74a0aba24a6a3
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2023-04-20 15:01:20 +0330

    das
    das

--- pm/settings.py
+++ pm/settings.py
@@ -16,6 +16,7 @@ from django.urls import reverse_lazy
 # from celery.schedules import crontab
 import logging.config
 from celery.schedules import crontab
+from pathlib import Path
 
 # import cmms.tasks
 
@@ -24,8 +25,8 @@ LOGGING_CONFIG = None
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
