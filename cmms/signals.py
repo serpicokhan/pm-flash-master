a538cd4b5ce3f7a74258f397c3f5af94ba7d4618
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2023-07-24 12:07:26 +0330

    das
    das

--- cmms/signals.py
+++ cmms/signals.py
@@ -167,10 +167,10 @@ def save_wo_profile(sender, instance, **kwargs):
             #         # if(user_token):
             #         #     push_notification.send_push(API_KEY,user_token
             #         #                            ,'دستور کار جدید',instance.summaryofIssue)
-            user_token=SysUser.objects.get(id=instance.assignToUser).token
+            user_token=SysUser.objects.get(id=instance.assignedToUser).token
             admin=SysUser.objects.get(id=1).token
             if(user_token):
-                some_function.delay(user_token,instance.summaryOfIssue);
+                some_function.delay(user_token,instance.assignedToUser);
             some_function.delay(admin,instance.summaryOfIssue);
 
     except Exception as e1:
