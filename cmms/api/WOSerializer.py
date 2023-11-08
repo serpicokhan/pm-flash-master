7880a4d201c2871ddf566f8ce5d423615c97aab1
moeinserpico <81881855+serpicokhan@users.noreply.github.com>
2023-11-08 17:06:42 +0330

    Update WOSerializer.py

--- cmms/api/WOSerializer.py
+++ cmms/api/WOSerializer.py
@@ -83,7 +83,7 @@ class MiniWorkorderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = WorkOrder
-        fields = ('summaryofIssue','woAsset','maintenanceType','woStatus','RequestedUser','timecreated')
+        fields = ('summaryofIssue','woAsset','maintenanceType','woStatus','RequestedUser','timecreated','datecreated','assignedToUser')
 
 class TaskSerializer(serializers.ModelSerializer):
     taskAssignedToUser = serializers.SlugRelatedField(
