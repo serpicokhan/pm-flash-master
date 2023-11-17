5a3583ef4c4a2fb8a9f31928947977c47bc1db08
amir2041 <morteza20413000@gmail.com>
2023-11-17 23:27:10 +0330

    Update admin.py

--- cmms/admin.py
+++ cmms/admin.py
@@ -4,3 +4,6 @@ from cmms.models import SysUser,TolidMoshakhase
 # Register your models here.
 admin.site.register(SysUser)
 admin.site.register(TolidMoshakhase)
+admin.site.site_header = 'پنل مدیریت اکسپرتر سرورایان'
+admin.site.site_title = 'پنل مدیریت اکسپرتر سرورایان'
+admin.site.index_title = 'به پنل مدیریت اکسپرتر سرو رایان  خوش آمدید'
