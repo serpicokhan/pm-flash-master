16dc89d1d76f56184eb03f7b7d86d011f0becd49
moein <morteza20413100@gmail.com>
2023-12-26 14:57:06 +0330

    Update token.py

--- cmms/views/token.py
+++ cmms/views/token.py
@@ -298,7 +298,8 @@ class SubSiteView(APIView):
 
 
     def get(self,request,id):
-            all_asstest=AssetUtility.get_sub_assets(id)
+            asset_id=Asset.objects.get(id=id)
+            all_asstest=AssetUtility.get_sub_assets(asset_id)
             # sites=Asset.objects.filter(Q(assetIsLocatedAt__id=id)|Q(assetIsPartOf__id=id)).filter(assetTypes=1)
             my_site=MiniAssetSerializer(all_asstest, many=True)
             return Response(my_site.data)
