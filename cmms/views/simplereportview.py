d8278cbcdf16645549f998ee0cb0edd580a115fd
moein <morteza20413100@gmail.com>
2024-02-13 19:33:32 +0330

    wopartusage
    add woid in 'woPartWorkorder for PartPlannedByLocation and PartUsageByLocation

--- cmms/views/simplereportview.py
+++ cmms/views/simplereportview.py
@@ -2343,7 +2343,7 @@ class reporttest:
         # if((assetname[0]==-1)):
         #      assetname=Asset.objects.values_list('id', flat=True)
         if(advanceMode):
-            n1=WorkorderPart.objects.values('woPartStock__stockItem__partName','woPartStock__id').filter(timeStamp__range=[date1,date2],woPartActulaQnty__gt=0)
+            n1=WorkorderPart.objects.values('woPartWorkorder__id','woPartStock__stockItem__partName','woPartStock__id').filter(timeStamp__range=[date1,date2],woPartActulaQnty__gt=0)
             template_name='cmms/reports/simplereports/PartUsageByLocation_acc.html'
             
         else:
@@ -2396,7 +2396,8 @@ class reporttest:
         else:
        
 
-            n1=WorkorderPart.objects.values('woPartWorkorder__woAsset__assetIsLocatedAt__assetName','woPartWorkorder__woAsset__assetName','woPartStock__stockItem__partName',
+            n1=WorkorderPart.objects.values('woPartWorkorder__id','woPartWorkorder__woAsset__assetIsLocatedAt__assetName',
+                                            'woPartWorkorder__woAsset__assetName','woPartStock__stockItem__partName',
             'woPartWorkorder__woAsset__assetCategory__name').filter(timeStamp__date__range=[date1,date2],woPartPlannedQnty__gt=0)
             template_name='cmms/reports/simplereports/PartPlannedByLocation.html'
         # print(n1.count(),'!!!!!!!!!!')
